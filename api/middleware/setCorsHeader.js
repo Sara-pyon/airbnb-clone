ec7f6ae56cae1667c0802745389616cabb7c93b0
Sara Komatsu <radwimps.sara@gmail.com>
2024-04-16 14:44:44 -0600

    Refactor: return error if catching error

--- api/middleware/setCorsHeader.js
+++ api/middleware/setCorsHeader.js
@@ -1,4 +1,4 @@
-module.exports = (req, res, next) => {
+module.exports = function(req, res, next){
     req.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     req.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     req.header('Access-Control-Allow-Credentials', true);
--- api/middleware/validator.js
+++ api/middleware/validator.js
@@ -1,7 +1,7 @@
 module.exports = (validator) => {
     return (req, res, next) => {
         const { error } = validator(req.body);
-        if(error) res.status(404).send(error.message);
+        if(error) return res.status(404).send(error.message);
         next();
     }
 }
